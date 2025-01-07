8b1d8524213fda4365277fc3cda88a2fb2b1f048
Jonas Thiago <146122741+jonas-thiago@users.noreply.github.com>
2025-01-07 15:19:13 -0300

    version1.1
    parte2

--- models/model.py
+++ models/model.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, SQLModel, create_engine
+from sqlmodel import Field, SQLModel, create_engine, Relationship
 from typing import Optional
 from datetime import date
 from decimal import Decimal
@@ -10,3 +10,8 @@ class Subscription(SQLModel, table=True):
     data_assinatura: date
     valor: Decimal
 
+class Payments(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    subscription_id: int = Field(foreign_key='subscription.id')
+    subscription: Subscription = Relationship()
+    date: date
--- views/views.py
+++ views/views.py
@@ -1,6 +1,8 @@
 import __init__
 from models.database import engine
-from models.model import Subscription
+from models.model import Subscription, Payments
+from sqlmodel import Session, select
+from datetime import date
 
 
 class SubscriptionService:
@@ -8,9 +10,25 @@ class SubscriptionService:
         self.engine = engine
 
     def create(self, subscription: Subscription):
-        ...
+        with Session(self.engine) as session:
+            session.add(subscription)
+            session.commit()
+            return subscription
+
+    def list_all(self):
+        with Session(self.engine) as session:
+            statement = select(Subscription)
+            results = session.exec(statement).all()
+        return results
+
+    def pay(self, subscription: Subscription):
+        with Session(self.engine) as session:
+            statement = select(Payments).where(
+                Subscription.empresa == subscription.empresa)
+            results = session.exec(statement).all()
 
 
 ss = SubscriptionService(engine)
-subscription = Subscription('netflix', 'netflix.com.br', '12/12/2024', 25)
-ss.create(subscription)
+# subscription = Subscription(empresa='Pythonando', site='pythonando.com.br', data_assinatura=date.today(), valor=35)
+# ss.create(subscription)
+print(ss.list_all())
